3158ce3353c918da8c93423b428c8c913846ebf2
Atila Fassina <hey@atilafassina.com>
2017-06-13 20:38:52 +0200

    add author section

---
+++ components/Author.js
@@ -0,0 +1,57 @@
+import { colors } from '../helpers/stylesConfig'
+
+export default ({ avatar_url, name, blog, bio }) => (
+  <div className="authorWrap">
+    <img className="avatar" src={avatar_url} alt={name} />
+    <section className="textWrap">
+      <strong className="name">{name}</strong>
+      <p className="bio">{bio}</p>
+      <a className="website" href={blog} target="_blank">{blog}</ a>
+    </section>
+
+    <style jsx>{`
+      .authorWrap {
+        width: 40vw;
+        height: 7rem;
+        padding-bottom: 5rem;
+        margin: 0 auto;
+        overflow: hidden;
+      }
+
+      .avatar {
+        float: left;
+        height: 100%;
+        border-radius: 50%;
+      }
+
+      .textWrap {
+        float: left;
+        height: 100%;
+        margin-left: 2rem;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        align-items: flex-start;
+      }
+      .name {
+        text-align: right;
+        font-size: 2rem;
+      }
+
+      .website {
+        display: block;
+        text-align: right;
+        text-decoration: none;
+        font-weight: bold;
+        cursor: pointer;
+        color: ${colors.text.darkGrey};
+        transition: all 250ms linear;
+      }
+
+      .website:focus,
+      .website:hover {
+        color: ${colors.simpleOrange}
+      }
+    `}</style>
+  </div>
+)
--- layouts/post.js
+++ layouts/post.js
@@ -1,54 +1,138 @@
+import React, { Component } from 'react'
 import Main from './main'
+import Author from '../components/Author'
 import { colors } from '../helpers/stylesConfig'
 import PrismStyles from '../static/vendor/prismstyles'
 
-const Post = ({ children }) => (<Main>
-  <script src="/static/vendor/prism.js"></script>
-  <article className="postWrapper">
-    { children }
-  </article>
-  <PrismStyles />
-  <style global jsx>{`
-    .postWrapper {
-      width: 70vw;
-      margin: 0 auto;
-      padding: 2em 0 3em;
-      line-height: 1.5;
-    }
+export default class Post extends Component {
+  constructor (props) {
+    super(props)
 
-    .postWrapper h2 {
-      text-align: center
+    this.state = {
+      author: 'Atila'
     }
+  }
 
-    .postWrapper h3 {
-      padding-top: 1.5em;
-    }
+  componentDidMount () {
+    this.requestAuthorData().then(data => {
 
-    .postWrapper a {
-      color: ${colors.text.darkGrey};
-      text-decoration: none;
-      font-weight: bold;
-      cursor: pointer;
-      transition: all 250ms linear;
-    }
+      this.setState({ author: data })
+    })
+  }
 
-    .postWrapper a:focus,
-    .postWrapper a:hover {
-      color: ${colors.simpleOrange}
-    }
+  requestAuthorData = async (username = 'atilafassina') => {
+    const response = await fetch(`http://api.github.com/users/${username}`)
+    return response.json()
+  }
 
-    .postWrapper pre {
-      width: 120%;
-      margin: 2rem 0;
-      margin-left: -10%;
-    }
+  render () {
+    const { children } = this.props
 
-    @media (min-width: 800px) {
-      .postWrapper {
-        width: 50vw;
-      }
-    }
-  `}</style>
-</Main>)
+    return (<Main>
+      <script src="/static/vendor/prism.js"></script>
+      <article className="postWrapper">
+        { children }
+      </article>
+      <Author {...this.state.author} />
+      <PrismStyles />
+      <style global jsx>{`
+        .postWrapper {
+          width: 70vw;
+          margin: 0 auto;
+          padding: 2em 0 3em;
+          line-height: 1.5;
+        }
 
-export default Post
+        .postWrapper h2 {
+          text-align: center
+        }
+
+        .postWrapper h3 {
+          padding-top: 1.5em;
+        }
+
+        .postWrapper a {
+          color: ${colors.text.darkGrey};
+          text-decoration: none;
+          font-weight: bold;
+          cursor: pointer;
+          transition: all 250ms linear;
+        }
+
+        .postWrapper a:focus,
+        .postWrapper a:hover {
+          color: ${colors.simpleOrange}
+        }
+
+        .postWrapper pre {
+          width: 120%;
+          margin: 2rem 0;
+          margin-left: -10%;
+        }
+
+        @media (min-width: 800px) {
+          .postWrapper {
+            width: 50vw;
+          }
+        }
+      `}</style>
+    </Main>)
+  }
+}
+// const Post = ({ children }) => {
+//   const author = requestAuthorData('atilafassina')
+//   author.then( data => this.setState(data))
+//   return (
+//     <Main>
+//       <script src="/static/vendor/prism.js"></script>
+//       <article className="postWrapper">
+//         { children }
+//       </article>
+//       <Author {...author} />
+//       <PrismStyles />
+//       <style global jsx>{`
+//         .postWrapper {
+//           width: 70vw;
+//           margin: 0 auto;
+//           padding: 2em 0 3em;
+//           line-height: 1.5;
+//         }
+
+//         .postWrapper h2 {
+//           text-align: center
+//         }
+
+//         .postWrapper h3 {
+//           padding-top: 1.5em;
+//         }
+
+//         .postWrapper a {
+//           color: ${colors.text.darkGrey};
+//           text-decoration: none;
+//           font-weight: bold;
+//           cursor: pointer;
+//           transition: all 250ms linear;
+//         }
+
+//         .postWrapper a:focus,
+//         .postWrapper a:hover {
+//           color: ${colors.simpleOrange}
+//         }
+
+//         .postWrapper pre {
+//           width: 120%;
+//           margin: 2rem 0;
+//           margin-left: -10%;
+//         }
+
+//         @media (min-width: 800px) {
+//           .postWrapper {
+//             width: 50vw;
+//           }
+//         }
+//       `}</style>
+//     </Main>
+//   )
+// }
+
+// export default Post
