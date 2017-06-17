2d9961646942384657e082a418b322f1c734ba8a
Atila Fassina <hey@atilafassina.com>
2017-06-17 17:05:56 +0200

    subtree deploy

---
+++ _next/cdf52db5-d09d-40b7-b431-023ecff695ed/page/2017/shared_components_monorepo/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/2017/shared_components_monorepo', function() {
+            var comp = module.exports=webpackJsonp([2],[],[288]);
+            return { page: comp.default }
+          })
+        
